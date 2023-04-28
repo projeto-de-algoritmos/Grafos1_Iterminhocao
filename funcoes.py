c3a4a437b71223032c4e05edcbd5a4b51cde5502
Pedro Nogueira <88006041+phnog@users.noreply.github.com>
2023-04-27 23:49:49 -0300

    Criação da Busca do Menor Caminho

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,4 @@
+from queue import Queue
 
 
 graph = {
@@ -11,21 +12,58 @@ graph = {
       "H" : ["G","F"],
       "I" : ["G"]
  }
-
 #
-visited = []
-queue = [] 
+#visited = []
+#queue = [] 
 
-def bfs(visited, graph, node):
-    visited.append(node)
-    queue.append(node)
+#def bfs(visited, graph, node):
+ #   visited.append(node)
+  #  queue.append(node)
 
-    while queue:
-        m = queue.pop(0)
-        print(m,end = " ")
+   # while queue:
+    #    m = queue.pop(0)
+     #   print(m,end = " ")
 
-        for neighbour in graph[m]:
-            if neighbour not in visited:
-                visited.append(neighbour)
-                queue.append(neighbour)
+      #  for neighbour in graph[m]:
+       #     if neighbour not in visited:
+        #        visited.append(neighbour)
+         #       queue.append(neighbour)
 
+
+def bfs(graph, inicial, final):
+    visited = {}
+    level = {}
+    parent = {}
+    bfs_transversal_output = []
+    queue = Queue()
+    
+    for no in graph.keys():
+        visited[no] = False
+        parent[no] = None
+        level[no] = -1
+    s = inicial
+    visited[s] = True
+    level[s] = 0
+    queue.put(s)
+
+    while not queue.empty():
+        u = queue.get()
+        bfs_transversal_output.append(u)
+
+        for v in graph[u]:
+            if not visited[v]:
+                visited[v] = True
+                parent[v] = u
+                level[v] = level[u]+1
+                queue.put(v)
+    print(bfs_transversal_output)
+    
+    f = final 
+    path = []
+    while f is not None: 
+        path.append(f)
+        f = parent[f]
+    path.reverse()
+    print(path)
+
+bfs(graph, "C", "A")
