f6914591e66db7c40730cdcf9df5456c63b75f6a
Pedro Nogueira <88006041+phnog@users.noreply.github.com>
2023-04-27 23:49:19 -0300

    Criação do BFS

--- funcoes.py
+++ funcoes.py
@@ -11,3 +11,21 @@ graph = {
       "H" : ["G","F"],
       "I" : ["G"]
  }
+
+#
+visited = []
+queue = [] 
+
+def bfs(visited, graph, node):
+    visited.append(node)
+    queue.append(node)
+
+    while queue:
+        m = queue.pop(0)
+        print(m,end = " ")
+
+        for neighbour in graph[m]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+
