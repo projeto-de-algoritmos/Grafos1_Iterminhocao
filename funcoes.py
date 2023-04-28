1fb2d8da39d27a2990526faa8c7a0dc2f64bb13f
Pedro Nogueira <88006041+phnog@users.noreply.github.com>
2023-04-27 23:48:32 -0300

    Criação do Grafo

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,4 @@
-from queue import Queue
+
 
 graph = {
       "A" : ["B","C"],
@@ -11,58 +11,3 @@ graph = {
       "H" : ["G","F"],
       "I" : ["G"]
  }
-#
-#visited = []
-#queue = [] 
-
-#def bfs(visited, graph, node):
- #   visited.append(node)
-  #  queue.append(node)
-
-   # while queue:
-    #    m = queue.pop(0)
-     #   print(m,end = " ")
-
-      #  for neighbour in graph[m]:
-       #     if neighbour not in visited:
-        #        visited.append(neighbour)
-         #       queue.append(neighbour)
-
-
-def bfs(graph, inicial, final):
-    visited = {}
-    level = {}
-    parent = {}
-    bfs_transversal_output = []
-    queue = Queue()
-    
-    for no in graph.keys():
-        visited[no] = False
-        parent[no] = None
-        level[no] = -1
-    s = inicial
-    visited[s] = True
-    level[s] = 0
-    queue.put(s)
-
-    while not queue.empty():
-        u = queue.get()
-        bfs_transversal_output.append(u)
-
-        for v in graph[u]:
-            if not visited[v]:
-                visited[v] = True
-                parent[v] = u
-                level[v] = level[u]+1
-                queue.put(v)
-    print(bfs_transversal_output)
-    
-    f = final 
-    path = []
-    while f is not None: 
-        path.append(f)
-        f = parent[f]
-    path.reverse()
-    print(path)
-
-bfs(graph, "C", "A")
